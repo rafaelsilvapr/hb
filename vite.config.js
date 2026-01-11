bf33cacaede30470e4e03b69b02b2b6432f579c7
Rafael Rodrigues da Silva <rafaelrodriguesdasilva@MacBook-Air-de-Rafael.local>
2026-01-11 03:08:04 -0300

    Fix navigation and asset paths for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/hb/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                quem_somos: resolve(__dirname, 'quem-somos.html'),
+            },
+        },
+    },
 })
